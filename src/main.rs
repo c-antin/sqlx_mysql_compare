9bc7f405e0fb3f33c2bfccaffdd6bb0179d8ffdd
Constatin Schellhammer <constantin.schellhammer@gmail.com>
2023-11-30 15:27:13 +0100

    setup dummy table

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{env, time::Instant};
 
 use anyhow::anyhow;
 use mysql::{prelude::*, Conn, Opts, Params};
-use sqlx::{Connection, MySqlConnection, QueryBuilder};
+use sqlx::{Connection, Executor, MySqlConnection, QueryBuilder};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -13,15 +13,34 @@ async fn main() -> anyhow::Result<()> {
     if arg1.is_none() {
         Err(anyhow!("arg1 missing!"))?;
     }
-    let n = arg1.unwrap().parse()?;
+    let n = arg1.unwrap().parse::<usize>()?;
+    //setup
+    {
+        let mut conn = MySqlConnection::connect(&url).await?;
+        conn.execute("CREATE DATABASE IF NOT EXISTS sqlx_mysql_compare")
+            .await?;
+        conn.execute("USE sqlx_mysql_compare").await?;
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS ids (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY)",
+        )
+        .await?;
+        conn.execute("TRUNCATE TABLE ids").await?;
+        let mut builder = QueryBuilder::new("INSERT INTO ids (id) ");
+        builder.push_values(vec![(); n], |mut b, _| {
+            b.push("DEFAULT");
+        });
+        let query = builder.build();
+        query.execute(&mut conn).await?;
+    }
     //mysql
     {
         let opts = Opts::from_url(&url)?;
         let mut conn = Conn::new(opts)?;
-        let queries = vec!["SELECT 1 FROM DUAL"; n];
+        let _ = conn.query_drop("USE sqlx_mysql_compare");
+        let query = format!("SELECT * FROM ids LIMIT {}", n);
         let ts = Instant::now();
         let len = {
-            let mut res = conn.exec_iter::<_, Params>(queries.join(" UNION ALL "), ().into())?;
+            let mut res = conn.exec_iter::<_, Params>(&query, ().into())?;
             let mut iter = res.iter().unwrap();
             let mut len = 0;
             while let Some(row) = iter.next() {
@@ -35,11 +54,8 @@ async fn main() -> anyhow::Result<()> {
     //sqlx
     {
         let mut conn = MySqlConnection::connect(&url).await?;
-        let mut queries = vec!["SELECT 1 FROM DUAL"; n];
-        let mut builder = QueryBuilder::new(queries.pop().unwrap());
-        for query in queries {
-            builder.push(&format!(" UNION ALL {}", query));
-        }
+        conn.execute("USE sqlx_mysql_compare").await?;
+        let mut builder = QueryBuilder::new(&format!("SELECT * FROM ids LIMIT {}", n));
         let query = builder.build();
         let ts = Instant::now();
         let len = {
